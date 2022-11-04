816acb57c3ccfe19ccf0000d8a3bec68f2a88836
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-11-04 16:57:11 +0100

    update

--- src/client.js
+++ src/client.js
@@ -19,6 +19,8 @@ import JSPath from 'jspath';
 /* CLIENT                                                                                                             */
 /*--------------------------------------------------------------------------------------------------------------------*/
 
+// noinspection JSUnusedGlobalSymbols
+
 /**
  * Class representing an AMI HTTP client
  */
@@ -45,6 +47,10 @@ class AMIHTTPClient
 	#converter = 'AMIXmlToJson.xsl';
 
 	#paramRegExp = new RegExp('-\\W*([a-zA-Z][a-zA-Z0-9]*)\\W*=\\W*\\?', 'g');
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	#errorMessage = 'resource temporarily unreachable';
 
 	/*----------------------------------------------------------------------------------------------------------------*/
 	/* METHODS                                                                                                        */
@@ -75,18 +81,23 @@ class AMIHTTPClient
 
 	/*----------------------------------------------------------------------------------------------------------------*/
 
+	static #error(message)
+	{
+		return {'AMIMessage': [{'error': [{'$': message}]}]};
+	}
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
 	/**
 	 * Executes an AMI command
 	 * @param {string} command the AMI command
-	 * @param {Object<string,*>} [options={}] dictionary of settings (endpoint, converter, extras, params, context, timeout)
-	 * @returns {$.Promise} A JQuery promise object
+	 * @param {Object<string,*>} [options={}] dictionary of settings (endpoint, converter, extras, params, timeout)
+	 * @returns {Promise} A JavaScript promise object
 	 */
 
 	execute(command, options)
 	{
 		options = options || {};
-
-		const result = $.Deferred();
 
 		/*------------------------------------------------------------------------------------------------------------*/
 
@@ -96,7 +107,6 @@ class AMIHTTPClient
 		const extras = options.extras || {};
 		const params = options.params || [];
 
-		const context = options.context || result;
 		const timeout = options.timeout || 120000;
 
 		/*------------------------------------------------------------------------------------------------------------*/
@@ -118,103 +128,147 @@ class AMIHTTPClient
 			...extras,
 		};
 
-		/*------------------------------------------------------------------------------------------------------------*/
-
-		const url               = endpoint                      ;
-		const urlWithParameters = endpoint + '?' + $.param(data);
+		const body = Object.entries(data).map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`).join('&');
 
 		/*------------------------------------------------------------------------------------------------------------*/
 
-		if(converter === 'AMIXmlToJson.xsl')
-		{
-			/*--------------------------------------------------------------------------------------------------------*/
-			/* JSON FORMAT                                                                                            */
-			/*--------------------------------------------------------------------------------------------------------*/
-
-			$.ajax({
-				url: url,
-				data: data,
-				type: 'POST',
-				timeout: timeout,
-				dataType: 'json',
-				xhrFields: {
-					withCredentials: true
-				},
-				success: (data) => {
-
-					const info = JSPath.apply('.AMIMessage.info.$', data);
-					const error = JSPath.apply('.AMIMessage.error.$', data);
-
-					if(error.length === 0)
-					{
-						result.resolveWith(context, [data, info.join('. '), urlWithParameters]);
-					}
-					else
-					{
-						result.rejectWith(context, [data, error.join('. '), urlWithParameters]);
-					}
-				},
-				error: (jqXHR, textStatus) => {
-
-					if(textStatus === ((('error'))))
-					{
-						textStatus = 'service temporarily unreachable';
-					}
-
-					if(textStatus === 'parsererror')
-					{
-						textStatus = 'resource temporarily unreachable';
-					}
-
-					const data = {'AMIMessage': [{'error': [{'$': textStatus}]}]};
-
-					result.rejectWith(context, [data, textStatus, urlWithParameters]);
-				},
-			});
-
-			/*--------------------------------------------------------------------------------------------------------*/
-		}
-		else
-		{
-			/*--------------------------------------------------------------------------------------------------------*/
-			/* OTHER FORMATS                                                                                          */
-			/*--------------------------------------------------------------------------------------------------------*/
-
-			$.ajax({
-				url: url,
-				data: data,
-				type: 'POST',
-				timeout: timeout,
-				dataType: 'text',
-				xhrFields: {
-					withCredentials: true
-				},
-				success: (data) => {
-
-					result.resolveWith(context, [data, data, urlWithParameters]);
-				},
-				error: (jqXHR, textStatus) => {
-
-					if(textStatus === 'error')
-					{
-						textStatus = 'service temporarily unreachable';
-					}
-
-					result.rejectWith(context, [textStatus, textStatus, urlWithParameters]);
-				},
-			});
-
-			/*--------------------------------------------------------------------------------------------------------*/
-		}
+		const url               = endpoint             ;
+		const urlWithParameters = endpoint + '?' + body;
 
 		/*------------------------------------------------------------------------------------------------------------*/
 
-		return result.promise();
+		return new Promise((resolve, reject) => {
+
+			if(converter === 'AMIXmlToJson.xsl')
+			{
+				/*----------------------------------------------------------------------------------------------------*/
+				/* JSON FORMAT                                                                                        */
+				/*----------------------------------------------------------------------------------------------------*/
+
+				let inTime = true;
+
+				const timeoutId = setTimeout(() => {
+
+					reject(AMIHTTPClient.#error('timeout'), 'timeout', urlWithParameters);
+
+					inTime = false;
+
+				}, timeout);
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				fetch(url, {
+					body: body,
+					method: 'POST',
+					headers: {
+						'Accept': 'application/json',
+						'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+					},
+					credentials: 'include',
+					cache: 'no-cache',
+					mode: 'cors',
+				}).finally(() => {
+
+					clearTimeout(timeoutId);
+
+				}).then((response) => {
+
+					if(inTime)
+					{
+						response.json().then((data) => {
+
+							const info = JSPath.apply('.AMIMessage.info.$', data);
+							const error = JSPath.apply('.AMIMessage.error.$', data);
+
+							if(error.length === 0)
+							{
+								resolve(data, info.join('. '), urlWithParameters);
+							}
+							else
+							{
+								reject(data, error.join('. '), urlWithParameters);
+							}
+
+						}).catch(() => {
+
+							reject(AMIHTTPClient.#error(this.#errorMessage), this.#errorMessage, urlWithParameters);
+						});
+					}
+
+				}).catch(() => {
+
+					if(inTime)
+					{
+						reject(AMIHTTPClient.#error(this.#errorMessage), this.#errorMessage, urlWithParameters);
+					}
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+			}
+			else
+			{
+				/*----------------------------------------------------------------------------------------------------*/
+				/* OTHER FORMATS                                                                                      */
+				/*----------------------------------------------------------------------------------------------------*/
+
+				let inTime = true;
+
+				const timeoutId = setTimeout(() => {
+
+					reject('timeout', 'timeout', urlWithParameters);
+
+					inTime = false;
+
+				}, timeout);
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				fetch(url, {
+					body: body,
+					method: 'POST',
+					headers: {
+						'Accept': 'text/plain',
+						'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+					},
+					credentials: 'include',
+					cache: 'no-cache',
+					mode: 'cors',
+				}).finally(() => {
+
+					clearTimeout(timeoutId);
+
+				}).then((response) => {
+
+					if(inTime)
+					{
+						response.text().then((data) => {
+
+							resolve(data, data, urlWithParameters);
+
+						}).catch(() => {
+
+							reject(this.#errorMessage, this.#errorMessage, urlWithParameters);
+						});
+					}
+
+				}).catch(() => {
+
+					if(inTime)
+					{
+						reject(this.#errorMessage, this.#errorMessage, urlWithParameters);
+					}
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+			}
+		});
+
+		/*------------------------------------------------------------------------------------------------------------*/
 	}
 
 	/*----------------------------------------------------------------------------------------------------------------*/
 
-	#guest()
+	static #guest()
 	{
 		return {
 			AMIUser: 'guest',
@@ -230,126 +284,116 @@ class AMIHTTPClient
 			lastName: 'guest',
 			email: 'N/A',
 			country: 'N/A',
-			valid: 'false',
-			certEnabled: 'false',
-			vomsEnabled: 'false',
+			valid: 'false'
 		}
 	}
 
 	/*----------------------------------------------------------------------------------------------------------------*/
 
 	/**
-	 * @param {$.Promise}
-	 * @param {Object<string,*>} [options={}]
-	 * @returns {$.Promise} A JQuery promise object
+	 * @param {Promise} promise
+	 * @returns {Promise} A JavaScript promise object
 	 */
 
-	#getUserInfo(deferred, options)
+	#getUserInfo(promise)
 	{
-		options = options || {};
-
-		const result = $.Deferred();
-
 		/*------------------------------------------------------------------------------------------------------------*/
 
-		const context = options.context || result;
+		return new Promise((resolve, reject) => {
 
-		/*------------------------------------------------------------------------------------------------------------*/
+			promise.then((data, message) => {
 
-		deferred.then((data, message) => {
+				const userInfo = {};
+				const roleInfo = {};
+				const bookmarkInfo = {};
+				const dashboardInfo = {};
+				const awfInfo = {};
 
-			const userInfo = {};
-			const roleInfo = {};
-			const bookmarkInfo = {};
-			const dashboardInfo = {};
-			const awfInfo = {};
+				/*--------------------------------------------------------------------------------------------------------*/
 
-			/*--------------------------------------------------------------------------------------------------------*/
+				JSPath.apply('..rowset{.@type==="user"}.row.field', data).forEach((item) => {
 
-			JSPath.apply('..rowset{.@type==="user"}.row.field', data).forEach((item) => {
-
-				userInfo[item['@name']] = item['$'];
-			});
-
-			/*--------------------------------------------------------------------------------------------------------*/
-
-			JSPath.apply('..rowset{.@type==="awf"}.row.field', data).forEach((item) => {
-
-				awfInfo[item['@name']] = item['$'];
-			});
-
-			/*--------------------------------------------------------------------------------------------------------*/
-
-			JSPath.apply('..rowset{.@type==="role"}.row', data).forEach((row) => {
-
-				let name = '';
-				const role = {};
-
-				row.field.forEach((field) => {
-
-					role[field['@name']] = field['$'];
-
-					if(field['@name'] === 'name')
-					{
-						name = field['$'];
-					}
+					userInfo[item['@name']] = item['$'];
 				});
 
-				roleInfo[name] = role;
-			});
+				/*--------------------------------------------------------------------------------------------------------*/
 
-			/*--------------------------------------------------------------------------------------------------------*/
+				JSPath.apply('..rowset{.@type==="awf"}.row.field', data).forEach((item) => {
 
-			JSPath.apply('..rowset{.@type==="bookmark"}.row', data).forEach((row) => {
-
-				let hash = '';
-				const bookmark = {};
-
-				row.field.forEach((field) => {
-
-					bookmark[field['@name']] = field['$'];
-
-					if(field['@name'] === 'hash')
-					{
-						hash = field['$'];
-					}
+					awfInfo[item['@name']] = item['$'];
 				});
 
-				bookmarkInfo[hash] = bookmark;
-			});
+				/*--------------------------------------------------------------------------------------------------------*/
 
-			/*--------------------------------------------------------------------------------------------------------*/
+				JSPath.apply('..rowset{.@type==="role"}.row', data).forEach((row) => {
 
-			JSPath.apply('..rowset{.@type==="dashboard"}.row', data).forEach((row) => {
+					let name = '';
+					const role = {};
 
-				let hash = '';
-				const dashboard = {};
+					row.field.forEach((field) => {
 
-				row.field.forEach((field) => {
+						role[field['@name']] = field['$'];
 
-					dashboard[field['@name']] = field['$'];
+						if(field['@name'] === 'name')
+						{
+							name = field['$'];
+						}
+					});
 
-					if(field['@name'] === 'hash')
-					{
-						hash = field['$'];
-					}
+					roleInfo[name] = role;
 				});
 
-				dashboardInfo[hash] = dashboard;
+				/*--------------------------------------------------------------------------------------------------------*/
+
+				JSPath.apply('..rowset{.@type==="bookmark"}.row', data).forEach((row) => {
+
+					let hash = '';
+					const bookmark = {};
+
+					row.field.forEach((field) => {
+
+						bookmark[field['@name']] = field['$'];
+
+						if(field['@name'] === 'hash')
+						{
+							hash = field['$'];
+						}
+					});
+
+					bookmarkInfo[hash] = bookmark;
+				});
+
+				/*--------------------------------------------------------------------------------------------------------*/
+
+				JSPath.apply('..rowset{.@type==="dashboard"}.row', data).forEach((row) => {
+
+					let hash = '';
+					const dashboard = {};
+
+					row.field.forEach((field) => {
+
+						dashboard[field['@name']] = field['$'];
+
+						if(field['@name'] === 'hash')
+						{
+							hash = field['$'];
+						}
+					});
+
+					dashboardInfo[hash] = dashboard;
+				});
+
+				/*--------------------------------------------------------------------------------------------------------*/
+
+				resolve(data, message, userInfo, roleInfo, bookmarkInfo, dashboardInfo, awfInfo);
+
+			}).catch((data, message) => {
+
+				reject(data, message, AMIHTTPClient.#guest(), {}, {}, {}, {});
 			});
-
-			/*--------------------------------------------------------------------------------------------------------*/
-
-			result.resolveWith(context, [data, message, userInfo, roleInfo, bookmarkInfo, dashboardInfo, awfInfo]);
-
-		}, (data, message) => {
-
-			result.rejectWith(context, [data, message, this.#guest(), {}, {}, {}, {}]);
 		});
 
 		/*------------------------------------------------------------------------------------------------------------*/
-
-		return result.promise();
 	}
 
 	/*----------------------------------------------------------------------------------------------------------------*/
@@ -357,15 +401,19 @@ class AMIHTTPClient
 	/**
 	 * Sign in by code
 	 * @param {string} code the code
-	 * @param {Object<string,*>} [options={}] dictionary of optional parameters (endpoint, converter, context, timeout)
-	 * @returns {$.Promise} A JQuery promise object
+	 * @param {Object<string,*>} [options={}] dictionary of optional parameters (endpoint, converter, timeout)
+	 * @returns {Promise} A JavaScript promise object
 	 */
 
 	signInByCode(code, options)
 	{
+		options = options || {};
+
+		options['extras'] = {'NoCert': null};
+		options['params'] = [code];
+
 		return this.#getUserInfo(
-			this.execute('GetSessionInfo -AMIUser="__oidc_code__" -AMIPass=?', {extras: {'NoCert': null}, params: [code]}),
-			options
+			this.execute('GetSessionInfo -AMIUser="__oidc_code__" -AMIPass=?', options)
 		);
 	}
 
@@ -374,15 +422,19 @@ class AMIHTTPClient
 	/**
 	 * Sign in by token
 	 * @param {string} token the token
-	 * @param {Object<string,*>} [options={}] dictionary of optional parameters (endpoint, converter, context, timeout)
-	 * @returns {$.Promise} A JQuery promise object
+	 * @param {Object<string,*>} [options={}] dictionary of optional parameters (endpoint, converter, timeout)
+	 * @returns {Promise} A JavaScript promise object
 	 */
 
 	signInByToken(token, options)
 	{
+		options = options || {};
+
+		options['extras'] = {'NoCert': null};
+		options['params'] = [token];
+
 		return this.#getUserInfo(
-			this.execute('GetSessionInfo -AMIUser="__oidc_token__" -AMIPass=?', {extras: {'NoCert': null}, params: [token]}),
-			options
+			this.execute('GetSessionInfo -AMIUser="__oidc_token__" -AMIPass=?', options)
 		);
 	}
 
@@ -392,15 +444,19 @@ class AMIHTTPClient
 	 * Sign in by password
 	 * @param {string} username the username
 	 * @param {string} password the password
-	 * @param {Object<string,*>} [options={}] dictionary of optional parameters (endpoint, converter, context, timeout)
-	 * @returns {$.Promise} A JQuery promise object
+	 * @param {Object<string,*>} [options={}] dictionary of optional parameters (endpoint, converter, timeout)
+	 * @returns {Promise} A JavaScript promise object
 	 */
 
 	signInByPassword(username, password, options)
 	{
+		options = options || {};
+
+		options['extras'] = {'NoCert': null};
+		options['params'] = [username, password];
+
 		return this.#getUserInfo(
-			this.execute('GetSessionInfo -AMIUser=? -AMIPass=?', {extras: {'NoCert': null}, params: [username, password]}),
-			options
+			this.execute('GetSessionInfo -AMIUser=? -AMIPass=?', options)
 		);
 	}
 
@@ -408,15 +464,19 @@ class AMIHTTPClient
 
 	/**
 	 * Sign in by certificate
-	 * @param {Object<string,*>} [options={}] dictionary of optional parameters (endpoint, converter, context, timeout)
-	 * @returns {$.Promise} A JQuery promise object
+	 * @param {Object<string,*>} [options={}] dictionary of optional parameters (endpoint, converter, timeout)
+	 * @returns {Promise} A JavaScript promise object
 	 */
 
 	signInByCertificate(options)
 	{
+		options = options || {};
+
+		options['extras'] = {};
+		options['params'] = [];
+
 		return this.#getUserInfo(
-			this.execute('GetSessionInfo', {extras: {}, params: []}),
-			options
+			this.execute('GetSessionInfo', options)
 		);
 	}
 
@@ -424,15 +484,19 @@ class AMIHTTPClient
 
 	/**
 	 * Sign out
-	 * @param {Object<string,*>} [options={}] dictionary of optional parameters (endpoint, converter, context, timeout)
-	 * @returns {$.Promise} A JQuery promise object
+	 * @param {Object<string,*>} [options={}] dictionary of optional parameters (endpoint, converter, timeout)
+	 * @returns {Promise} A JavaScript promise object
 	 */
 
 	signOut(options)
 	{
+		options = options || {};
+
+		options['extras'] = {'NoCert': null};
+		options['params'] = [(((''))), ((('')))];
+
 		return this.#getUserInfo(
-			this.execute('GetSessionInfo -AMIUser=? -AMIPass=?', {extras: {'NoCert': null}, params: [(((''))), ((('')))]}),
-			options
+			this.execute('GetSessionInfo -AMIUser=? -AMIPass=?', options)
 		);
 	}
 
